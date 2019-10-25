f6b037507736f1b2834412dfb9e8ce5b583d219b
Your Name <you@example.com>
2019-10-25 17:47:12 +0800

    123

--- demo6.py
+++ demo6.py
@@ -1,9 +1,8 @@
 import sys, os
 sys.path.append(os.pardir)
-from tensorflow.examples.tutorials.mnist import load_mnist
+from tensorflow.examples.tutorials.mnist
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True,normalize=False)
 print(x_train.shape) # (60000, 784)
 print(t_train.shape) # (60000,)
 print(x_test.shape) # (10000, 784)
 print(t_test.shape) # (10000,)
-
