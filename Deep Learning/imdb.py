b128cec7831a0521fc3c7de20fec5b7a4b29c630
Your Name <yangfulin123@qq.com>
2020-04-17 16:07:01 +0800

    delete

---
+++ Deep Learning/imdb.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Created on 2020/4/17 13:46
+Update  on 2020/4/17 13:46
+Author: 不告诉你
+Software: PyCharm
+GitHub: https://github.com/Saber891
+"""
---
+++ Deep Learning/mnist.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Created on 2020/4/17 10:16
+Update  on 2020/4/17 10:16
+Author: 不告诉你
+Software: PyCharm
+GitHub: https://github.com/Saber891
+"""
