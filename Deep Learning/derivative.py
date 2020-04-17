df4505f9a622e82a4397d502b6ae5d375ae29a1b
Your Name <yangfulin123@qq.com>
2020-04-17 10:10:51 +0800

    随机梯度下降

--- Deep Learning/derivative.py
+++ Deep Learning/derivative.py
@@ -7,3 +7,14 @@ Author: 不告诉你
 Software: PyCharm
 GitHub: https://github.com/Saber891
 """
+import numpy as np
+import tensorflow as tf
+
+past_velocity = 0.
+momentum = 0.1
+while loss > 0.01:
+    w,loss,gradient = get_current_parameters()
+    velocity = past_velocity * momentum - leraning_rate * gradient
+    w = w + momentum * velocity - learning_rate * gradient
+    past_velocity = velocity
+    update_parameter(w)
