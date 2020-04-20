daeacd90fe0dbe5ee95fd5282864a428e64b9a5c
lin <yangfulin123@qq.com>
2020-04-20 11:28:25 +0800

    新闻分类

--- Deep Learning/imdb.py
+++ Deep Learning/imdb.py
@@ -20,6 +20,8 @@ import matplotlib.pyplot as plt
 word_index = imdb.get_word_index()
 train_data
 test_data
+
+#仅为评论翻译，与主程序无关
 reversed_word_index = dict([(value, key) for (key, value) in word_index.items()])
 decoded_review = ' '.join([reversed_word_index.get(i - 3, '?') for i in train_data[0]])
 
@@ -30,34 +32,32 @@ def vectorize_sequences(sequences, dimesion=10000):
         results[i, sequences] = 1.
     return results
 
-train_data
-test_data
 
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
-
-x_test
-x_train
 
 y_train = np.asanyarray(train_labels).astype('float32')
 y_test = np.asanyarray(test_labels).astype('float32')
 
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
-# model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
 
-# model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy,metrics=[metrics.binary_accuracy])
+model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy,
+              metrics=[metrics.binary_accuracy])
 x_val = x_train[:10000]
 partial_x_train = x_train[10000:]
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+history = model.fit(partial_x_train, partial_y_train, epochs=50, batch_size=512, validation_data=(x_val, y_val))
 history_dict = history.history
 history_dict.keys()
 
---
+++ Deep Learning/news.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Created on 2020/4/20 10:53
+Update  on 2020/4/20 10:53
+Author: 不告诉你
+Software: PyCharm
+GitHub: https://github.com/Saber891
+"""
+
+from keras.datasets import reuters
+from keras import models
+from keras import layers
+from keras.utils.np_utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequences in enumerate(sequences):
+        results[i, sequences] = 1.
+    return results
+
+
+x_train = vectorize_sequences(train_data)  # 训练数据向量化
+x_test = vectorize_sequences(test_data)  # 测试数据向量化
+
+ont_hot_train_labels = to_categorical(train_labels)
+ont_hot_test_labels = to_categorical(test_labels)
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+y_val = ont_hot_train_labels[:1000]
+partial_y_train = ont_hot_train_labels[1000:]
+
+history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, loss, 'bo', label="Traning Loss")
+plt.plot(epochs, val_loss, 'b', label="Validation Loss")
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.clf()
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+plt.plot(epochs,acc,'bo',label="Training acc")
+plt.plot(epochs,val_acc,'b',label= "Validation Loss")
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
