24782258fef24e05ae194e3c48c6cefbbeda0a1a
lin <yangfuiln123@qq.com>
2020-04-20 09:43:32 +0800

    keras练习

--- Deep Learning/imdb.py
+++ Deep Learning/imdb.py
@@ -18,6 +18,8 @@ import matplotlib.pyplot as plt
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 word_index = imdb.get_word_index()
+train_data
+test_data
 reversed_word_index = dict([(value, key) for (key, value) in word_index.items()])
 decoded_review = ' '.join([reversed_word_index.get(i - 3, '?') for i in train_data[0]])
 
@@ -28,9 +30,14 @@ def vectorize_sequences(sequences, dimesion=10000):
         results[i, sequences] = 1.
     return results
 
+train_data
+test_data
 
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
+
+x_test
+x_train
 
 y_train = np.asanyarray(train_labels).astype('float32')
 y_test = np.asanyarray(test_labels).astype('float32')
@@ -40,18 +47,17 @@ model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
 
-model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy,
-              metrics=[metrics.binary_accuracy])
+# model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy,metrics=[metrics.binary_accuracy])
 x_val = x_train[:10000]
 partial_x_train = x_train[10000:]
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-history = model.fit(partial_x_train, partial_y_train, epochs=50, batch_size=512, validation_data=(x_val, y_val))
+history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
 history_dict = history.history
 history_dict.keys()
 
